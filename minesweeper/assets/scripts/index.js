0f59d2ec3c406c8c4a92e6f389dede4016cef733
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-05-18 00:27:55 +0300

    init: start task

---
+++ minesweeper/assets/scripts/index.js
@@ -0,0 +1,10 @@
+/* created by sheritsh // Oleg Polovinko ※ RS School / School 21, Kzn  */
+
+class MinesweeperGame {
+  constructor() {
+    // generate initial page elements
+    const main = (document.body.innerHTML = 'mnswpr');
+  }
+}
+
+const game = new MinesweeperGame();
