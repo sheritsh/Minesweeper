bc92569552b07af09cfda250838ca8ecfc2654c8
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-05-19 01:23:06 +0300

    feat: create generating bomb func

--- minesweeper/assets/scripts/index.js
+++ minesweeper/assets/scripts/index.js
@@ -1,5 +1,10 @@
 /* created by sheritsh // Oleg Polovinko ※ RS School / School 21, Kzn  */
 
+function getRandInt(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
 class MinesweeperGame {
   constructor(domElement) {
     // fields
@@ -7,9 +12,12 @@ class MinesweeperGame {
     this.y_size = 10;
     this.timerCounter = 0;
     this.turnsCounter = 0;
-    this.bomsAmount = 0;
+    this.bombsAmount = 10;
     this.bombsLeftCounter = 0;
     this.flagsCounter = 0;
+    this.gameSize = this.x_size * this.y_size;
+    this.gameArray = new Array(this.gameSize);
+    this.gameArray.fill('0');
     // generate initial page elements
     this.gameBoard = document.createElement('div');
     this.gameBoard.classList.add('board');
@@ -25,11 +33,25 @@ class MinesweeperGame {
   }
 
   fillField() {
-    for (let i = 0; i < this.x_size * this.y_size; i++) {
+    for (let i = 0; i < this.gameSize; i++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.setAttribute('id', i.toString());
       this.gameField.append(cell);
+    }
+  }
+
+  generateGameAray() {
+    let unplacedBombs = this.bombsAmount;
+
+    for (let i = 0; unplacedBombs; i++) {
+      let randInt = getRandInt(0, this.gameSize - 1);
+      console.log(randInt);
+      if (this.gameArray[randInt] !== 'b') {
+        this.gameArray[randInt] = 'b';
+        unplacedBombs--;
+      }
+      console.log(this.gameArray[randInt]);
     }
   }
 }
@@ -43,3 +65,5 @@ gameWrapper.append(gameTitle);
 
 const game = new MinesweeperGame(gameWrapper);
 game.fillField();
+game.generateGameAray();
+console.log(game.gameArray);
