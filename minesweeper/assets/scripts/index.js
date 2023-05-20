7363dfd7e69dc9ebd39a213dfb0ae00f4e16f560
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-05-20 03:58:18 +0300

    feat: neigbors counter + bomb placement

--- minesweeper/assets/scripts/index.js
+++ minesweeper/assets/scripts/index.js
@@ -16,8 +16,9 @@ class MinesweeperGame {
     this.bombsLeftCounter = 0;
     this.flagsCounter = 0;
     this.gameSize = this.x_size * this.y_size;
-    this.gameArray = new Array(this.gameSize);
-    this.gameArray.fill('0');
+    this.gameArray = [];
+    this.initializeArray();
+    this.generateGameAray();
     // generate initial page elements
     this.gameBoard = document.createElement('div');
     this.gameBoard.classList.add('board');
@@ -32,11 +33,115 @@ class MinesweeperGame {
     this.gameBoard.append(this.gameField);
   }
 
+  initializeArray() {
+    for (let i = 0; i < this.y_size; i++) {
+      const arr = new Array(this.x_size);
+      arr.fill('0');
+      this.gameArray.push(arr);
+    }
+    console.log(this.gameArray);
+  }
+
+  getNeigborsBombsCount(cellId) {
+    console.log(cellId);
+    const cellColumn = Math.floor(cellId % this.x_size);
+    const cellRow = Math.floor(cellId / this.x_size);
+    let bombsAroundCounter = 0;
+
+    // console.log('Search for Y: ' + cellRow + ' X: ' + cellColumn);
+
+    for (let i = cellRow - 1; i <= cellRow + 1; i++) {
+      if (i < 0 || i > this.y_size - 1) continue;
+      for (let j = cellColumn - 1; j <= cellColumn + 1; j++) {
+        if (j < 0 || j > this.x_size - 1) continue;
+        if (i === cellRow && j === cellColumn) continue;
+        // console.log('Search for Y: ' + i + ' X: ' + j);
+        if (this.gameArray[i][j] === 'b') {
+          bombsAroundCounter++;
+        }
+      }
+    }
+
+    return bombsAroundCounter;
+  }
+
+  openEmptyNeigbors(cellId) {
+    const cellColumn = Math.floor(cellId % this.x_size);
+    const cellRow = Math.floor(cellId / this.x_size);
+
+    for (let i = cellRow - 1; i <= cellRow + 1; i++) {
+      if (i < 0 || i > this.y_size - 1) continue;
+      for (let j = cellColumn - 1; j <= cellColumn + 1; j++) {
+        if (j < 0 || j > this.x_size - 1) continue;
+        if (i === cellRow && j === cellColumn) continue;
+        if (this.gameArray[i][j] === '0' && this.getNeigborsBombsCount(cellId) === 0) {
+          const curCell = document.getElementById(cellId.toString());
+          const curId = i * this.x_size + j;
+          curCell.classList.add('cell-clicked');
+          // this.openEmptyNeigbors(curId);
+          // setTimeout(() => {
+          //   this.openEmptyNeigbors(curId);
+          // }, 1000);
+        }
+      }
+    }
+  }
+
+  revealCell(event) {
+    const clickedCell = event.target;
+    const clickedCellId = clickedCell.getAttribute('id');
+    const cellColumn = Math.floor(clickedCellId % this.x_size);
+    const cellRow = Math.floor(clickedCellId / this.x_size);
+
+    if (this.gameArray[cellRow][cellColumn] === 'b') {
+      clickedCell.classList.add('cell-bomb');
+    } else {
+      // clickedCell.classList.add('cell-clicked');
+      switch (this.getNeigborsBombsCount(clickedCellId)) {
+        case 1:
+          clickedCell.classList.add('cell-one');
+          break;
+        case 2:
+          clickedCell.classList.add('cell-two');
+          break;
+        case 3:
+          clickedCell.classList.add('cell-three');
+          break;
+        case 4:
+          clickedCell.classList.add('cell-four');
+          break;
+        case 5:
+          clickedCell.classList.add('cell-five');
+          break;
+        case 6:
+          clickedCell.classList.add('cell-six');
+          break;
+        case 7:
+          clickedCell.classList.add('cell-seven');
+          break;
+        case 8:
+          clickedCell.classList.add('cell-eight');
+          break;
+        default:
+          this.openEmptyNeigbors(clickedCellId);
+          break;
+      }
+    }
+  }
+
   fillField() {
     for (let i = 0; i < this.gameSize; i++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.setAttribute('id', i.toString());
+      cell.addEventListener('click', (event) => {
+        this.revealCell(event);
+      });
+      cell.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        cell.classList.add('cell-flag');
+        // TO DO: function to handle flag placement
+      });
       this.gameField.append(cell);
     }
   }
@@ -44,15 +149,18 @@ class MinesweeperGame {
   generateGameAray() {
     let unplacedBombs = this.bombsAmount;
 
-    for (let i = 0; unplacedBombs; i++) {
-      let randInt = getRandInt(0, this.gameSize - 1);
-      console.log(randInt);
-      if (this.gameArray[randInt] !== 'b') {
-        this.gameArray[randInt] = 'b';
+    while (unplacedBombs) {
+      let randXInt = getRandInt(0, this.x_size - 1);
+      let randYInt = getRandInt(0, this.y_size - 1);
+      console.log('Y: ' + randYInt + ' X: ' + randXInt);
+      console.log(this.gameArray[randYInt][randXInt]);
+      if (this.gameArray[randYInt][randXInt] !== 'b') {
+        this.gameArray[randYInt][randXInt] = 'b';
         unplacedBombs--;
       }
-      console.log(this.gameArray[randInt]);
     }
+
+    console.log(this.gameArray);
   }
 }
 
@@ -65,5 +173,4 @@ gameWrapper.append(gameTitle);
 
 const game = new MinesweeperGame(gameWrapper);
 game.fillField();
-game.generateGameAray();
-console.log(game.gameArray);
+// game.generateGameAray();
